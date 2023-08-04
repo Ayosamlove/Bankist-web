f3137e6a3e01f252b28271232d56928b7fd87a8d
Dammy Knight <dammyknight@pc-137-174.customer.ask4.lan>
2023-08-04 18:50:35 +0100

    Intersection observer for revealing sections implemented

--- script.js
+++ script.js
@@ -171,6 +171,24 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 headerObserver.observe(header);
 
+//Reveal sections
+const revealSection = function (entries, observer) {
+	const [entry] = entries;
+
+	if (!entry.isIntersecting) return;
+	entry.target.classList.remove('section--hidden');
+	observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+	root: null,
+	threshold: 0.15,
+});
+allSections.forEach((section) => {
+	sectionObserver.observe(section);
+	section.classList.add('section--hidden');
+});
+
 //cookies
 // Passing "argument" into handler
 
