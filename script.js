d6a125c4d57c36a2b28442ffd6d8403fd3cba939
Dammy Knight <dammyknight@pc-137-174.customer.ask4.lan>
2023-08-05 00:49:58 +0100

    Building a slider component: part 1

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 });
 allSections.forEach((section) => {
 	sectionObserver.observe(section);
-	section.classList.add('section--hidden');
+	// section.classList.add('section--hidden');
 });
 
 //Lazy loading images
@@ -212,6 +212,51 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 imgTargets.forEach((img) => {
 	imgObserver.observe(img);
+});
+
+//slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots');
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+// const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+const goToSlide = function (slide) {
+	slides.forEach((s, i) => {
+		s.style.transform = `translateX(${100 * (i - slide)}%)`;
+	});
+};
+
+goToSlide(0);
+
+//Next slide
+const nextSlide = function () {
+	if (curSlide === maxSlide - 1) {
+		curSlide = 0;
+	} else curSlide++;
+
+	goToSlide(curSlide);
+};
+
+const previousSlide = function () {
+	if (curSlide === 0) {
+		curSlide = maxSlide - 1;
+	} else curSlide--;
+	goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', previousSlide);
+
+document.addEventListener('keydown', function (e) {
+	if (e.key === 'ArrowLeft') previousSlide();
+	e.key === 'ArrowRight' && nextSlide();
 });
 
 //cookies
